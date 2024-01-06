eed3ab56c8c0acaadaa7d3f1d44065dd5ab7900e
ErenYeager04 <parkerviktorov094@gmail.com>
2024-01-06 17:03:09 +0100

    End of section 10

--- API/Data/Migrations/20240104150422_OrderEntityAdded.Designer.cs
+++ API/Data/Migrations/20240104150422_OrderEntityAdded.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace API.Data.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    partial class StoreContextModelSnapshot : ModelSnapshot
+    [Migration("20240104150422_OrderEntityAdded")]
+    partial class OrderEntityAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.14");
@@ -246,10 +249,10 @@ namespace API.Data.Migrations
                     b.Property<int>("Id")
                         .HasColumnType("INTEGER");
 
-                    b.Property<string>("Address1")
+                    b.Property<string>("Addres2")
                         .HasColumnType("TEXT");
 
-                    b.Property<string>("Address2")
+                    b.Property<string>("Address1")
                         .HasColumnType("TEXT");
 
                     b.Property<string>("City")
@@ -397,10 +400,10 @@ namespace API.Data.Migrations
                             b1.Property<int>("OrderId")
                                 .HasColumnType("INTEGER");
 
-                            b1.Property<string>("Address1")
+                            b1.Property<string>("Addres2")
                                 .HasColumnType("TEXT");
 
-                            b1.Property<string>("Address2")
+                            b1.Property<string>("Address1")
                                 .HasColumnType("TEXT");
 
                             b1.Property<string>("City")
---
+++ API/Data/Migrations/20240106140611_errorFixed.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class errorFixed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Addres2",
+                table: "UserAddress",
+                newName: "Address2");
+
+            migrationBuilder.RenameColumn(
+                name: "ShippingAddress_Addres2",
+                table: "Orders",
+                newName: "ShippingAddress_Address2");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Address2",
+                table: "UserAddress",
+                newName: "Addres2");
+
+            migrationBuilder.RenameColumn(
+                name: "ShippingAddress_Address2",
+                table: "Orders",
+                newName: "ShippingAddress_Addres2");
+        }
+    }
+}
